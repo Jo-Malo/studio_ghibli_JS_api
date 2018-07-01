e3601003b0d2b791967159ee921ce5c69789f10c
Jo Malo <juanitamalo.p@gmail.com>
2018-07-01 20:59:41 +0100

    comments added

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -4,17 +4,20 @@ const SelectView = function (selectElement) {
   this.selectElement = selectElement;
 };
 
+// populating menu after subscribing to dates ready channel in films.js after get
 SelectView.prototype.subscribeToDatesAndPopulateDropDown = function () {
   PubSub.subscribe('Films:release-dates-ready', (evt) => {
     this.populateSelect(evt.detail);
   });
 
+// recording year selection and publish index to SelectView channel
 this.selectElement.addEventListener('change', (evt) => {
   const selectedIndex = evt.target.value;
   PubSub.publish('SelectView:change', selectedIndex);
   });
 };
 
+// Runs through releaseDate array and initial add to menu option
 SelectView.prototype.populateSelect = function (releaseDates) {
   releaseDates.forEach((release_date, index) => {
     const option = this.createReleaseDateOption(release_date, index);
@@ -23,6 +26,7 @@ SelectView.prototype.populateSelect = function (releaseDates) {
   })
 };
 
+// cretes initial menu option with date and index
 SelectView.prototype.createReleaseDateOption = function (release_date, index) {
   const option = document.createElement('option');
   option.textContent = release_date;
